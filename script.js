109170c9d912aa9eebafa17f5bf662d98adb7c4b
Gyanprakash <gyanprakashdangi@gmail.com>
2023-01-28 17:43:24 +0530

    extra styling added

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-let buttons=document.querySelector('.buttons')
+let buttons=document.querySelector('.buttons');
+let btn=document.querySelectorAll('span');
+let value=document.getElementById('value');
+
+for (let i = 0; i < btn.length; i++) {
+      btn[i].addEventListener("click",function(){
+
+        if(this.innerHTML=="="){
+            value.innerHTML=eval(value.innerHTML);
+            
+        }
+        else{
+            if(this.innerHTML=="Clear"){
+                value.innerHTML="";
+            }
+            else{
+                value.innerHTML +=this.innerHTML;
+            }
+        }
+      }) 
+}
